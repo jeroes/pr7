cf17195eccb10285a034d836cb0415fe70b082f3
jeroes <>
2015-11-04 20:24:46 +0100

    Jesus Rodriguez

--- src/pr7app/Pr7app.java
+++ src/pr7app/Pr7app.java
@@ -1,9 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pr7app;
+
+import it.sauronsoftware.ftp4j.FTPAbortedException;
+import it.sauronsoftware.ftp4j.FTPClient;
+import it.sauronsoftware.ftp4j.FTPDataTransferException;
+import it.sauronsoftware.ftp4j.FTPException;
+import it.sauronsoftware.ftp4j.FTPIllegalReplyException;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 
 /**
  *
@@ -14,10 +25,32 @@ public class Pr7app {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        System.out.println("Hola profes!");
+    public static void main(String[] args) throws MalformedURLException, IOException, FTPIllegalReplyException, IllegalStateException, FTPException, FileNotFoundException, FTPDataTransferException, FTPAbortedException {
+                
+            URL url = new URL("http://192.168.122.200/docs/ic10-m04-WindowsServer.pdf");
+            url.openConnection();
+            InputStream leer = url.openStream();
+            FileOutputStream escribir = new FileOutputStream("jesus.pdf");
+            byte[] buffer = new byte[153600];
+            int bytesRead = 0;
+            while ((bytesRead = leer.read(buffer)) > 0) {
+                escribir.write(buffer, 0, bytesRead);
+            }
+            escribir.close();
+            leer.close();
+            
+            System.out.println("Descargando fichero...");
+            System.out.println("Fichero descargado correctamente.");
+            System.out.println("Subiendo fichero...");
+            
+            FTPClient clienteFTP = new FTPClient();
+            clienteFTP.connect("srv.toca.cat",21);
+            clienteFTP.login("fulano", "Platano123");
+            clienteFTP.upload(new java.io.File("jesus.pdf"));
+            clienteFTP.disconnect(true);
+            System.out.println("Fichero subido correctamente.");
     }
-    
+        
+       
+  
 }
